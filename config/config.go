a97cb48c57d807b81cdea3a2596ecfa1cd3e4b5c
刘斐 <liufei@04nl.com>
2024-01-25 15:08:24 +0800

    fix：mysql数据库初始化

--- config/config.go
+++ config/config.go
@@ -3,6 +3,8 @@ package config
 import "gin_api/internal"
 
 func InitConfig() {
-	//internal.InitDB()//数据库
-	internal.InitViper() //获取配置文件相关函数
+	internal.InitDB() //数据库
+}
+func init() {
+	internal.InitViper()
 }
--- internal/viper.go
+++ internal/viper.go
@@ -13,13 +13,12 @@ func InitViper() {
 	if env == "" {
 		env = "test"
 	}
-	viper.SetConfigName(fmt.Sprintf("config_%s", env)) //配置文件文件名
-	viper.SetConfigType("yml")                         //配置文件的类型
-	viper.AddConfigPath(workDir + "/config")           //工作目录
+	viper.SetConfigName(fmt.Sprintf("%s", env)) //配置文件文件名
+	viper.SetConfigType("yml")                  //配置文件的类型
+	viper.AddConfigPath(workDir + "/config")    //工作目录
 	err := viper.ReadInConfig()
 	if err != nil {
 		zap.S().Error("ReadInConfig err:" + err.Error())
-		//log.WaymonLogger.Error("ReadInConfig err:" + err.Error())
 		fmt.Println("ReadInConfig err:" + err.Error())
 	}
 	fmt.Println("Viper 初始化成功")
