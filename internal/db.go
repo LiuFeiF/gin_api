894b1f91c4ccc83cdea6e64461d3bf84f794e1ad
刘斐 <liufei@04nl.com>
2024-01-25 16:01:29 +0800

    fix：mysql数据库初始化配置修改。

--- internal/db.go
+++ internal/db.go
@@ -14,7 +14,7 @@ import (
 var (
 	DB        *gorm.DB
 	WriteConn string
-	ReadConn1 string
+	_         string
 )
 
 func InitDB() {
@@ -30,9 +30,10 @@ func InitDB() {
 	if gin.Mode() == "debug" {
 		gormLogger = logger.Default.LogMode(logger.Info)
 	} else {
-		gormLogger = logger.Default
+		gormLogger = logger.Default //根据当前的运行模式（通过gin.Mode()获取），设置日志记录级别。如果是调试模式，则将日志记录级别设置为Info，否则使用默认级别
 	}
-	_, err := gorm.Open(mysql.New(mysql.Config{
+	//通过gorm.Open函数打开MySQL数据库连接，并传入gorm.Config进行一些额外的配置，比如日志记录和命名策略。主要这里是为了更清晰的展示我们运行的sql语句
+	db, err := gorm.Open(mysql.New(mysql.Config{
 		DSN:                       WriteConn,
 		DefaultStringSize:         256,   //数据库字符串最大长度
 		DisableDatetimePrecision:  true,  //禁用datetime精度
@@ -48,5 +49,17 @@ func InitDB() {
 	if err != nil {
 		fmt.Println("db Open err:", err)
 	}
+	DB = db //这个地方主要是把gorm功能加入到数据库中//成功打开数据库连接后，将连接赋值给全局变量DB，以便后续在应用程序中使用。
+	////主从配置//这里其实可以进行数据库的均衡配置一个数据库进行写一个进行读，或者一主多从，但是我就一个数据库就先这样把
+	//_ = DB.Use(dbresolver.
+	//	Register(dbresolver.Config{
+	//		Sources: []gorm.Dialector{
+	//			mysql.Open(WriteConn), //写操作
+	//		},
+	//		Replicas: []gorm.Dialector{
+	//			mysql.Open(WriteConn), //读操作
+	//		},
+	//		Policy: dbresolver.RandomPolicy{}, //负载均衡政策
+	//	}).SetMaxIdleConns(10).SetMaxOpenConns(30).SetConnMaxLifetime(time.Hour * 1))
 	fmt.Println("mysql 初始化成功")
 }
